d5f80d522ace15cd50bd01f3cf4a47a3e5256177
Mizuki <jvcerezo@up.edu.ph>
2024-05-14 00:18:14 +0800

    implemented base code for authentication

--- elbigayan/lib/main.dart
+++ elbigayan/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:elbigayan/pages/sign_in.dart ';
 import 'firebase_options.dart';
+import 'providers/auth_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
