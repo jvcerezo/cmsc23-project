141d9ff3224baa88bfca1c6d6f6372cd7c0e5279
Andrae <147961288+devMapute@users.noreply.github.com>
2024-05-13 14:08:09 +0800

    modified donation details ui for org view

--- elbigayan/lib/pages/organization/orgview_donation_page.dart
+++ elbigayan/lib/pages/organization/orgview_donation_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:elbigayan/models/donation_model.dart';
+import 'package:flutter/widgets.dart';
 
 class DonationDetailsPage extends StatelessWidget {
   final Donation donation;
@@ -10,13 +11,43 @@ class DonationDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Donation Details'),
+        titleSpacing: 40,
+        toolbarHeight: 100,
+        automaticallyImplyLeading: false,
+        actions: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 0, 30, 0),
+            width: 30,
+            height: 30,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.close),
+              color: Colors.blue[900],
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              iconSize: 24,
+              padding: EdgeInsets.zero,
+              alignment: Alignment.center,
+            ),
+          ),
+        ],
+        title: const Text(
+          'Donation Details',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue[900],
       ),
       body: Center(
+        
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(height: 50,),
             Text('User ID: ${donation.userID}'),
             Text('Category: ${donation.category}'),
             Text('Mode: ${donation.mode}'),
