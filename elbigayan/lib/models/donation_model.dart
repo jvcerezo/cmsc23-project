acb5b77ce6c8d537b0f7d2d6c7ea41042ed9bf57
Andrae <147961288+devMapute@users.noreply.github.com>
2024-05-13 12:08:37 +0800

    added a donation model

--- elbigayan/lib/models/donation_model.dart
+++ elbigayan/lib/models/donation_model.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-class Donation{
+class Donation {
   final int userID;
   String? id;
   String category;
@@ -12,17 +10,40 @@ class Donation{
   Timestamp deliveryTime;
   String imageUrl;
 
-  Donation({
-    required this.userID,
-    this.id,
-    required this.category,
-    required this.mode,
-    required this.weight,
-    required this.deliveryTime,
-    required this.imageUrl
-  });
+  Donation(
+      {required this.userID,
+      this.id,
+      required this.category,
+      required this.mode,
+      required this.weight,
+      required this.deliveryTime,
+      required this.imageUrl});
 
+  factory Donation.fromJson(Map<String, dynamic> json) {
+    return Donation(
+        userID: json['userID'],
+        id: json['id'],
+        category: json['category'],
+        mode: json['mode'],
+        weight: json['weight'],
+        deliveryTime: json['deliveryTime'],
+        imageUrl: json['imageUrl']);
+  }
 
+  static List<Donation> fromJsonArray(String jsonData) {
+    final Iterable<dynamic> data = jsonDecode(jsonData);
+    return data.map<Donation>((dynamic d) => Donation.fromJson(d)).toList();
+  }
 
-
+  Map<String, dynamic> toJson(Donation donation) {
+    return {
+      'userID': donation.userID,
+      'id': donation.id,
+      'category': donation.category,
+      'mode': donation.mode,
+      'weight': donation.mode,
+      'deliveryTime': donation.deliveryTime,
+      'imageUrl': donation.imageUrl
+    };
+  }
 }
