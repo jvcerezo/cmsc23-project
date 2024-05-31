6fc8b6e7edaf4d6fead8eea279a8b88eb0da80bb
RamBenitez <ribenitez@up.edu.ph>
2024-05-31 23:27:01 +0800

    style:add logo in sign up page

--- elbigayan/lib/pages/auth/signup.dart
+++ elbigayan/lib/pages/auth/signup.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Sign Up")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/signup-donor');
-              },
-              child: const Text("Sign Up as Donor"),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/signup-organization');
-              },
-              child: const Text("Sign Up as Organization"),
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.only(bottom: 150),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/logo.gif'),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/signup-donor');
+                },
+                child: const Text("Sign Up as Donor"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamed('/signup-organization');
+                },
+                child: const Text("Sign Up as Organization"),
+              ),
+            ],
+          ),
         ),
       ),
     );
